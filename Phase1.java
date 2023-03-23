13341c84964bd82b1ffbc2406429ff230007ccec
Ranchy101 <42690717+Ranchy101@users.noreply.github.com>
2023-03-22 21:05:10 -0700

    Add files via upload

--- Phase1.java
+++ Phase1.java
@@ -1,5 +1,6 @@
 package comp440project1;
 import java.sql.*;
+import java.time.LocalDate;
 
 import javax.swing.JOptionPane;
 
@@ -22,6 +23,41 @@ public class Phase1 {
 		              + "email VARCHAR(100) UNIQUE"
 		              + ")";
 		      stmt.executeUpdate(createTableSQL);
+		      String createItemTable = "CREATE TABLE IF NOT EXISTS Item ("
+                      + "id INT AUTO_INCREMENT PRIMARY KEY,"
+                      + "title VARCHAR(255),"
+                      + "description TEXT,"
+                      + "category VARCHAR(255),"
+                      + "price DECIMAL(10,2),"
+                      + "username VARCHAR(255),"
+                      + "FOREIGN KEY (username) REFERENCES user(username))";
+		      stmt.executeUpdate(createItemTable);
+		      String createReviewTable = "CREATE TABLE IF NOT EXISTS Review ("
+                      + "id INT AUTO_INCREMENT PRIMARY KEY,"
+                      + "rating ENUM('excellent', 'good', 'fair', 'poor'),"
+                      + "description TEXT,"
+                      + "username VARCHAR(255),"
+                      + "itemId INT,"
+                      + "FOREIGN KEY (username) REFERENCES user(username),"
+                      + "FOREIGN KEY (itemId) REFERENCES Item(id))";
+		      stmt.executeUpdate(createReviewTable);
+		      String createPostLimitTable = "CREATE TABLE IF NOT EXISTS PostLimit ("
+                      + "id INT AUTO_INCREMENT PRIMARY KEY,"
+                      + "username VARCHAR(255),"
+                      + "postCount INT DEFAULT 0,"
+                      + "lastPostDate DATE,"
+                      + "FOREIGN KEY (username) REFERENCES user(username))";
+		      stmt.executeUpdate(createPostLimitTable);
+		      String createReviewPostingTable = "CREATE TABLE IF NOT EXISTS ReviewPosting ("
+                      + "id INT AUTO_INCREMENT PRIMARY KEY,"
+                      + "username VARCHAR(255),"
+                      + "itemId INT,"
+                      + "reviewId INT,"
+                      + "postingDate DATE,"
+                      + "FOREIGN KEY (username) REFERENCES user(username),"
+                      + "FOREIGN KEY (itemId) REFERENCES Item(id),"
+                      + "FOREIGN KEY (reviewId) REFERENCES Review(id))";
+		      stmt.executeUpdate(createReviewPostingTable);
 		      stmt.close();
 		  } catch (SQLException ex) {
 		      	System.out.println("SQL error: " + ex.getMessage());
@@ -30,11 +66,32 @@ public class Phase1 {
 	  public static void clearTables() throws SQLException {
 	    try {
 		    Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
-		    
+      
+	     // Disable foreign key constraints
+	        Statement disableFKStmt = conn.createStatement();
+	        disableFKStmt.execute("SET foreign_key_checks = 0");
+	        disableFKStmt.close();
+	        
+	        // Delete data from tables
 	        Statement stmt = conn.createStatement();
 	        String clearTableSQL = "DELETE FROM user";
 	        stmt.executeUpdate(clearTableSQL);
+	        String clearItemTable = "DELETE FROM Item";
+	        stmt.executeUpdate(clearItemTable);
+	        String clearReviewTable = "DELETE FROM Review";
+	        stmt.executeUpdate(clearReviewTable);
+	        String clearPostLimitTable = "DELETE FROM PostLimit";
+	        stmt.executeUpdate(clearPostLimitTable);
+	        String clearReviewLimitTable = "DELETE FROM ReviewPosting";
+	        stmt.executeUpdate(clearReviewLimitTable);
 	        stmt.close();
+	        
+	        // Re-enable foreign key constraints
+	        Statement enableFKStmt = conn.createStatement();
+	        enableFKStmt.execute("SET foreign_key_checks = 1");
+	        enableFKStmt.close();
+	        
+	        conn.close();
 	    } catch (SQLException ex) {
 		      System.out.println("SQL error: " + ex.getMessage());
 	      }
@@ -73,7 +130,7 @@ public class Phase1 {
 	    }
 		return 0;
 	  }
-	  
+	  public static String loggedUser;
 	  public static int loginUser(String username, String password) {
 		  try {
 		      Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
@@ -88,6 +145,7 @@ public class Phase1 {
 		      if (rs.next()) {
 		    	 // User logged in successfully
 		    	  conn.close(); 
+		    	  loggedUser = username;
 		    	  return 1;
 		      }
 		      else {
@@ -100,24 +158,197 @@ public class Phase1 {
 	    }
 		return 0;
 	  }
+	//-----------------------------------------PHASE 2 METHODS--------------------------------------------------------------------------------
+	  public static int insertItem(String title, String description, String category, double price, String username) {
+		    try {
+		        Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+
+		        // Check if the user has reached the limit of 3 posts for the day
+		        PreparedStatement limitStmt = conn.prepareStatement("SELECT SUM(postCount) as postCount FROM PostLimit WHERE username = ? AND lastPostDate = ?");
+		        limitStmt.setString(1, username);
+		        limitStmt.setDate(2, new java.sql.Date(System.currentTimeMillis()));
+		        ResultSet limitRs = limitStmt.executeQuery();
+
+		        int postCount = 0;
+		        if (limitRs.next()) {
+		            postCount = limitRs.getInt("postCount");
+		        }
+
+		        if (postCount >= 3) {
+		            // User has reached the limit of 3 posts for the day
+		            conn.close();
+		            return 0;
+		        }
+
+		        // Insert the item into the Item table
+		        PreparedStatement itemStmt = conn.prepareStatement("INSERT INTO Item (title, description, category, price, username) VALUES (?, ?, ?, ?, ?)");
+		        itemStmt.setString(1, title);
+		        itemStmt.setString(2, description);
+		        itemStmt.setString(3, category);
+		        itemStmt.setDouble(4, price);
+		        itemStmt.setString(5, username);
+		        int itemAffectedRows = itemStmt.executeUpdate();
+
+		        if (itemAffectedRows == 1) {
+		            // Update the post count in the PostLimit table
+		            PreparedStatement limitUpdateStmt = conn.prepareStatement("INSERT INTO PostLimit (username, postCount, lastPostDate) VALUES (?, ?, ?) ON DUPLICATE KEY UPDATE postCount = ?");
+		            limitUpdateStmt.setString(1, username);
+		            limitUpdateStmt.setInt(2, 1);
+		            limitUpdateStmt.setDate(3, new java.sql.Date(System.currentTimeMillis()));
+		            limitUpdateStmt.setInt(4, postCount + 1);
+		            int limitAffectedRows = limitUpdateStmt.executeUpdate();
+
+		            if (limitAffectedRows == 1) {
+		                // Item inserted successfully and post count updated
+		                conn.close();
+		                return 1;
+		            }
+		        }
+
+		        // Failed to insert item or update post count
+		        conn.close();
+		        return 0;
+		    } catch (SQLException ex) {
+		        System.out.println("SQL error: " + ex.getMessage());
+		    }
+		    return 0;
+		}
+	  public static String searchItem(String searchQuery) {
+		    String result = "";
+		    try {
+		        Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+
+		        // Use INSTR function to search for searchQuery in each column
+		        PreparedStatement stmt = conn.prepareStatement("SELECT * FROM Item WHERE INSTR(title, ?) > 0 OR INSTR(description, ?) > 0 OR INSTR(category, ?) > 0");
+		        stmt.setString(1, searchQuery);
+		        stmt.setString(2, searchQuery);
+		        stmt.setString(3, searchQuery);
+
+		        ResultSet rs = stmt.executeQuery();
+		        while (rs.next()) {
+		            result += "Title: " + rs.getString("title") + "\n";
+		            result += "Description: " + rs.getString("description") + "\n";
+		            result += "Category: " + rs.getString("category") + "\n";
+		            result += "Price: " + rs.getDouble("price") + "\n";
+		            result += "ID: " + rs.getInt("id")+ "\n\n";
+		        }
+		        conn.close();
+		        if (result.equals("")) {
+		            result = "No results found.";
+		        }
+		        return result;
+		    } catch (SQLException ex) {
+		        System.out.println("SQL error: " + ex.getMessage());
+		        result = "No results found.";
+		        return result;
+		    }
+		}
+	  public static int giveReview(String username, int itemId, String rating, String description) {
+		    try {
+		        Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+
+		        // check if user has already posted 3 reviews for the day
+		        LocalDate currentDate = LocalDate.now();
+		        PreparedStatement checkStmt = conn.prepareStatement("SELECT COUNT(*) FROM ReviewPosting WHERE username = ? AND postingDate = ?");
+		        checkStmt.setString(1, username);
+		        checkStmt.setDate(2, Date.valueOf(currentDate));
+		        ResultSet rs = checkStmt.executeQuery();
+		        rs.next();
+		        int postCount = rs.getInt(1);
+		        if (postCount >= 3) {
+		            conn.close();
+		            return -1; // user has already reached the daily limit
+		        }
+
+		        // check if user is trying to review their own item
+		        PreparedStatement selfReviewStmt = conn.prepareStatement("SELECT COUNT(*) FROM Item WHERE id = ? AND username = ?");
+		        selfReviewStmt.setInt(1, itemId);
+		        selfReviewStmt.setString(2, username);
+		        ResultSet selfReviewRs = selfReviewStmt.executeQuery();
+		        selfReviewRs.next();
+		        int selfReviewCount = selfReviewRs.getInt(1);
+		        if (selfReviewCount >= 1) {
+		            conn.close();
+		            return -2; // user cannot review their own item
+		        }
+		        
+		        // start a transaction
+		        conn.setAutoCommit(false);
+
+		        // insert review into Review table
+		        PreparedStatement reviewStmt = conn.prepareStatement("INSERT INTO Review (rating, description, username, itemId) VALUES (?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+		        reviewStmt.setString(1, rating);
+		        reviewStmt.setString(2, description);
+		        reviewStmt.setString(3, username);
+		        reviewStmt.setInt(4, itemId);
+		        int reviewResult = reviewStmt.executeUpdate();
+
+		        if (reviewResult == 1) {
+		            // get the generated review ID
+		            ResultSet generatedKeys = reviewStmt.getGeneratedKeys();
+		            generatedKeys.next();
+		            int reviewId = generatedKeys.getInt(1);
+
+		            // update post count for user
+		            PreparedStatement updateStmt = conn.prepareStatement("INSERT INTO PostLimit (username, postCount, lastPostDate) \r\n"
+		            		+ "VALUES (?, 1, ?) \r\n"
+		            		+ "ON DUPLICATE KEY UPDATE postCount = CASE \r\n"
+		            		+ "                                        WHEN postCount < 3 THEN postCount + 1 \r\n"
+		            		+ "                                        ELSE postCount \r\n"
+		            		+ "                                    END, \r\n"
+		            		+ "                                    lastPostDate = ?");
+		            updateStmt.setString(1, username);
+		            updateStmt.setDate(2, Date.valueOf(currentDate));
+		            updateStmt.setDate(3, Date.valueOf(currentDate));
+		            updateStmt.executeUpdate();
+
+		            // insert review posting record
+		            PreparedStatement postingStmt = conn.prepareStatement("INSERT INTO ReviewPosting (username, itemId, reviewId, postingDate) VALUES (?, ?, ?, ?)");
+		            postingStmt.setString(1, username);
+		            postingStmt.setInt(2, itemId);
+		            postingStmt.setInt(3, reviewId);
+		            postingStmt.setDate(4, Date.valueOf(currentDate));
+		            postingStmt.executeUpdate();
+
+		            // commit the transaction
+		            conn.commit();
+		            conn.close();
+		            return 1; // review added successfully
+		        }
+
+		        // rollback the transaction if any step fails
+		        conn.rollback();
+		        conn.close();
+		        return 0; // review failed to add
+		    } catch (SQLException ex) {
+		        System.out.println("SQL error: " + ex.getMessage());
+		    }
+
+		    return 0;
+		}
+	  
+	  
 	  
 	  // -----------------------------------TESTING------------------------------------------
 	  public static void main(String[] args) throws SQLException {
 		//create database (also re-creates)
-		initializeDatabase();  
+		//initializeDatabase();  
 	    
 		// register a new user
-	    registerUser("johnsmith", "password123", "John", "Smith", "johnsmith@example.com");
+	    //registerUser("johnsmith", "password123", "John", "Smith", "johnsmith@example.com");
 	    
 	    // try to register a user with the same username
-	    registerUser("johnsmith", "password456", "John", "Doe", "johndoe@example.com");
+	    //registerUser("johnsmith", "password456", "John", "Doe", "johndoe@example.com");
 	    
 	    // test login correct username and password
-	    loginUser("johnsmith", "password123");
+	    //loginUser("johnsmith", "password123");
 	    
 	    // test login incorrect username and password
-	    loginUser("johnsmith", "wrongpassword");
-	    
+	    //loginUser("johnsmith", "wrongpassword");
+	    //insertItem("Smartphone", "new iphone X", "electronic, cellphone, apple", 1000, loggedUser); //AND CODE THAT STOPS AFTER 3 POSTS A DAY
+	    //insertItem("Smartphone2", "new iphone X", "electronic, cellphone, apple", 1000, loggedUser);//HAVE TO ADD CODE THAT SAVES LAST LOGGED IN USERNAME 
+	    //searchItem("electronic");
+	    //giveReview(loggedUser,16, "excellent", "perfect. exactly what i want");
 	    //initializeDatabase();
 	  }
 }
--- Phase1GUI.java
+++ Phase1GUI.java
@@ -9,6 +9,18 @@ import java.sql.SQLException;
 public class Phase1GUI extends JFrame implements ActionListener {
     private JTextField usernameFieldLogin, passwordFieldLogin, firstNameField, lastNameField, emailField,usernameFieldRegister,passwordFieldRegister,confirmPasswordFieldRegister;
     private JButton loginButton, registerButton, initializeButton;
+    private JLabel loginHeading, usernameLabelLogin, passwordLabelLogin, usernameLabelRegister, passwordLabelRegister, confirmPasswordLabelRegister, firstNameLabel, lastNameLabel, emailLabel, registerHeading, searchHeading;
+    private JLabel insertHeading, titleLabel, descriptionLabel, categoryLabel, priceLabel;
+    
+    private JTextField titleField, descriptionField, categoryField, priceField, searchField;
+    private JButton insertButton, searchButton;
+    
+    private JLabel reviewHeading, reviewLabel, reviewDescription, rating;
+    private JTextField reviewSearch, description;
+    private JButton reviewButton;
+    private JComboBox<String> dropdown;
+    
+    private JLabel spacer;
 
     public Phase1GUI() {
         // Set up the GUI components
@@ -17,37 +29,37 @@ public class Phase1GUI extends JFrame implements ActionListener {
         setLayout(new GridLayout(21, 3));
         
         //login
-        JLabel loginHeading = new JLabel("Login", SwingConstants.CENTER);
+         loginHeading = new JLabel("Login", SwingConstants.CENTER);
         
-        JLabel usernameLabelLogin = new JLabel("Username: ");
+         usernameLabelLogin = new JLabel("Username: ");
         usernameFieldLogin = new JTextField(20);
 
-        JLabel passwordLabelLogin = new JLabel("Password: ");
+         passwordLabelLogin = new JLabel("Password: ");
         passwordFieldLogin = new JTextField(20);
         
         //register
-        JLabel usernameLabelRegister = new JLabel("Username: ");
+         usernameLabelRegister = new JLabel("Username: ");
         usernameFieldRegister = new JTextField(20);
 
-        JLabel passwordLabelRegister = new JLabel("Password: ");
+         passwordLabelRegister = new JLabel("Password: ");
         passwordFieldRegister = new JTextField(20);
         
-        JLabel confirmPasswordLabelRegister = new JLabel("Confirm Password: ");
+         confirmPasswordLabelRegister = new JLabel("Confirm Password: ");
         confirmPasswordFieldRegister = new JTextField(20);
 
-        JLabel firstNameLabel = new JLabel("First Name: ");
+         firstNameLabel = new JLabel("First Name: ");
         firstNameField = new JTextField(20);
         
-        JLabel lastNameLabel = new JLabel("Last Name: ");
+         lastNameLabel = new JLabel("Last Name: ");
         lastNameField = new JTextField(20);
         
-        JLabel emailLabel = new JLabel("Email: ");
+         emailLabel = new JLabel("Email: ");
         emailField = new JTextField(20);
         
         loginButton = new JButton("Login");
         loginButton.addActionListener(this);
 
-        JLabel registerHeading = new JLabel("Register", SwingConstants.CENTER);
+        registerHeading = new JLabel("Register", SwingConstants.CENTER);
         registerButton = new JButton("Register User");
         registerButton.addActionListener(this);
 
@@ -83,8 +95,38 @@ public class Phase1GUI extends JFrame implements ActionListener {
 
         pack();
         setVisible(true);
-    }
+        
+       // ---------------------------------------INSERT ITEM-----------------------------------
+         insertHeading = new JLabel("SELLING", SwingConstants.CENTER);
 
+         titleLabel = new JLabel("Title: ");
+        titleField = new JTextField(20);
+
+         descriptionLabel = new JLabel("Description: ");
+        descriptionField = new JTextField(20);
+
+         categoryLabel = new JLabel("Category: ");
+        categoryField = new JTextField(20);
+
+         priceLabel = new JLabel("Price: ");
+        priceField = new JTextField(20);
+
+        insertButton = new JButton("Insert Item");
+        insertButton.addActionListener(this);
+        
+        searchButton = new JButton("Search Item");
+        searchButton.addActionListener(this);
+        
+       reviewButton = new JButton("Review Item");
+       reviewHeading = new JLabel("Reviewing", SwingConstants.CENTER);
+       reviewSearch = new JTextField(20);
+       reviewLabel = new JLabel("Item ID: ");
+       reviewDescription = new JLabel("Description:");
+       description = new JTextField(20);
+       rating = new JLabel("Rating:");
+       reviewButton.addActionListener(this);
+    }
+    public String loggedUser;
     @Override
     public void actionPerformed(ActionEvent e) {
     	
@@ -93,8 +135,91 @@ public class Phase1GUI extends JFrame implements ActionListener {
             String username = usernameFieldLogin.getText();
             String password = passwordFieldLogin.getText();
             int x = Phase1.loginUser(username, password);
-            if (x == 1)
+            if (x == 1) {
             	JOptionPane.showMessageDialog(this, "Logged in successfully.");
+            	loggedUser = username;
+            	// Create a new JPanel
+                JPanel newPanel = new JPanel(new GridLayout(2, 2));
+
+                // Add UI elements for new menu to new JPanel
+                //JLabel welcomeLabel = new JLabel("Welcome " + username + "!", SwingConstants.CENTER);
+               // JButton logoutButton = new JButton("Logout");
+               // logoutButton.addActionListener(this);
+
+               // newPanel.add(welcomeLabel);
+                //newPanel.add(logoutButton);
+
+                // Remove UI elements for login from the current panel
+                remove(loginHeading);
+                remove(usernameLabelLogin);
+                remove(usernameFieldLogin);
+                remove(passwordLabelLogin);
+                remove(passwordFieldLogin);
+                remove(loginButton);
+                remove(usernameLabelRegister);
+                remove(passwordLabelRegister);
+                remove(usernameFieldRegister);
+                remove(passwordFieldRegister);
+                remove(confirmPasswordLabelRegister);
+                remove(confirmPasswordFieldRegister);
+                remove(firstNameLabel);
+               	remove(firstNameField);
+               	remove(lastNameLabel);
+               	remove(lastNameField);
+               	remove(emailLabel);
+               	remove(emailField);
+                remove(loginButton);
+                remove(registerHeading);
+                remove(registerButton);
+                remove(initializeButton);
+                
+                
+                // Add the new JPanel to the current panel
+                add(newPanel);
+
+                // Update the current panel
+                revalidate();
+                repaint();
+                add(insertHeading);
+                add(titleLabel);
+                add(titleField);
+                add(descriptionLabel);
+                add(descriptionField);
+                add(categoryLabel);
+                add(categoryField);
+                add(priceLabel);
+                add(priceField);
+                add(insertButton);
+                
+                searchHeading = new JLabel("Searching", SwingConstants.CENTER);
+                JLabel searchLabel = new JLabel("Category: ");
+                searchField = new JTextField(20);
+                
+                add(spacer = new JLabel(" "),"span, grow");
+                
+                add(searchHeading);
+                add(searchLabel);
+                add(searchField);
+                add(searchButton);
+                
+                add(spacer = new JLabel(" "),"span, grow");
+                
+                add(reviewHeading);
+                add(reviewLabel);
+                add(reviewSearch);
+                add(rating);
+                String[] options = {"Excellent", "Good", "Fair", "Poor"};
+                dropdown = new JComboBox<>(options);
+                add(dropdown);
+                add(reviewDescription);
+                add(description);
+                add(reviewButton);
+                
+                setLayout(new GridLayout(25, 3));
+                
+                pack();
+                setVisible(true);
+            }
             else
             	JOptionPane.showMessageDialog(this, "Invalid username or password.");
         }
@@ -131,8 +256,62 @@ public class Phase1GUI extends JFrame implements ActionListener {
                 JOptionPane.showMessageDialog(this, "Error initializing database: " + ex.getMessage());
             }
         }
+        //insert item button
+        else if (e.getSource() == insertButton) {
+            String title = titleField.getText();
+            String description = descriptionField.getText();
+            String category = categoryField.getText();
+            String priceStr = priceField.getText();
+            double price = Double.parseDouble(priceStr);
+            int x = Phase1.insertItem(title, description, category, price, loggedUser );
+            if (x == 1) {
+            	JOptionPane.showMessageDialog(this, "Item inserted successfully.");
+            }
+            else 
+            	JOptionPane.showMessageDialog(this, "You have reached your post cap of 3 for today.");
+        }
+      //search item button
+        else if (e.getSource() == searchButton) {
+            String searched = searchField.getText();
+            String searchResults = Phase1.searchItem(searched);
+            createResultWindow(searchResults);
+        }
+        //review item button
+        else if (e.getSource() == reviewButton) {
+            String itemIdString = reviewSearch.getText();
+            int itemID = Integer.parseInt(itemIdString);
+            String descriptionText = description.getText();
+            String ratingOption = (String) dropdown.getSelectedItem();
+            int x = Phase1.giveReview(loggedUser, itemID, ratingOption, descriptionText);
+            if (x == 1) {
+            	JOptionPane.showMessageDialog(this, "Review sent successfully.");
+            }
+            else if (x == -1 ){
+            	JOptionPane.showMessageDialog(this, "You have reached your review cap of 3 for today.");
+            }
+            else if (x == -2 ){
+            	JOptionPane.showMessageDialog(this, "You cannot review your own post.");
+            }
+            else
+            	System.out.println(x);
+        }
     }
+    //for printing search results
+    public void createResultWindow(String results) {
+        JFrame resultFrame = new JFrame("Search Results");
+        resultFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
+        JTextArea resultTextArea = new JTextArea(results);
+        resultTextArea.setEditable(false);
+
+        JScrollPane scrollPane = new JScrollPane(resultTextArea);
+        resultFrame.getContentPane().add(scrollPane);
+
+        
+        
+        resultFrame.pack();
+        resultFrame.setVisible(true);
+    }
     public static void main(String[] args) {
         new Phase1GUI();
     }
